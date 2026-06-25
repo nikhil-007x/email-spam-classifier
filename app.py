660a5510c903a873b871fc04b2c54b185d93bee1
nikhil-007x <3nikhilkumar03032005@gmail.com>
2026-06-25 13:44:52 +0530

    adding nltk stopwords

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+
+
 
 ps = PorterStemmer()
 
